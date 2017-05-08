c74db6e2064ad6ff19516ea1a007ce57d1ac7746
Ana Raquel Campesan <anazard@outlook.com>
2017-05-08 09:54:49 -0400

    added examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Accessing element properties';
+
+  constructor(private renderer: Renderer) { }
+
+  // Reference to the element
+  @ViewChild('myDiv') myDiv: ElementRef;
+
+  // Work with it within after view inits to guarantee the element is found before being used
+  ngAfterViewInit() {
+    this.renderer.setElementClass(this.myDiv.nativeElement, 'test-class', true);
+  }
+
+  @ViewChild('myInput') myInput: ElementRef;
+  focus() {
+    this.renderer.invokeElementMethod(this.myInput.nativeElement, 'focus');
+  }
+
+  height() {
+    // Setting properties
+    console.log(this.myDiv.nativeElement.style.height);
+    this.renderer.setElementStyle(this.myDiv.nativeElement, 'height', '300px');
+    console.log(this.myDiv.nativeElement.style.height);
+    this.renderer.setElementStyle(this.myDiv.nativeElement, 'backgroundColor', 'blue');
+    // Reading properties
+    console.log(this.myDiv.nativeElement.offsetHeight);
+  }
 }
